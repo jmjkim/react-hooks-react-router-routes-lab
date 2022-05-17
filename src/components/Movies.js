33029d64dbed69dd1b4e78d1f2a973901d3d3043
Jongmin Jack Kim <jmjkim00@icloud.com>
2022-05-17 02:02:04 -0700

    Completed assignment

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,25 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const listOfMovies = movies.map((movie) => {
+    return (
+      <div key={movie.title}>
+        <h3>Name: {movie.title}</h3>
+        <p>Time: {movie.time}</p>
+        <p>Genres:</p>
+        <ul>
+          {movie.genres.map((genre) => <li key={genre}>{genre}</li>)}
+        </ul>
+      </div>
+    );
+  })
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {listOfMovies}
+    </div>
+  );
 }
 
 export default Movies;
